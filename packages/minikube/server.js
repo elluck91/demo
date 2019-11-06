373d4f3045672d60790ab1b50cec8eb4577597f1
Lukasz Juraszek <elluck91@localhost.localdomain>
2019-11-06 15:56:05 -0800

    Interesting changes

--- packages/minikube/server.js
+++ packages/minikube/server.js
@@ -3,8 +3,8 @@ var http = require('http');
 var handleRequest = function (request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World, from minikube 1');
+  response.end('Hello World, from minikube1');
 };
 var www = http.createServer(handleRequest);
-www.listen(8000);
-console.log('Server is listening od port 8000.');
+www.listen(8001);
+console.log('Server is listening od port 8001.');
--- packages/minikube2/server.js
+++ packages/minikube2/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function (request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World, from minikube2, yea!!');
+  response.end('Hello World, from minikube2!!');
 };
 var www = http.createServer(handleRequest);
-www.listen(8080);
+www.listen(8002);
