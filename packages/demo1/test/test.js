67dbcc00f3e74ba3229bfdfbb1e7b6a20084458b
elluck91 <juraszeklukasz@gmail.com>
2019-11-14 00:30:42 -0800

    Added tests

--- packages/demo1/test/test.js
+++ packages/demo1/test/test.js
@@ -5,7 +5,6 @@ var request = require("request"),
 
 describe("DEMO_1 Server", function() {
   describe("GET /", function() {
-    this.timeout(1000);
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
         assert.equal(200, response.statusCode);
