297f9f0a7028483d7bfc9e6d973ae745a181cccd
Lukasz Juraszek <juraszeklukasz@gmail.com>
2019-11-15 12:20:40 -0800

    changes to demo2

--- packages/demo2/server.js
+++ packages/demo2/server.js
@@ -4,11 +4,11 @@ var exports = module.exports = {};
 const PORT = 9002;
 
 app.get('/', function (req, res) {
-  res.send('DEMO_2');
+  res.send('DEMO_2 on branch master');
 });
 
 var server = app.listen(PORT, function () {
-  console.log(` Magic is happening on port ${PORT}`);
+  console.log(`Magic is happening on port ${PORT}`);
 });
 
 exports.closeServer = function () {
--- packages/demo2/test/test.js
+++ packages/demo2/test/test.js
@@ -1,20 +1,20 @@
 var request = require("request"),
-    assert = require('assert'),
-    demo1 = require("../server.js"),
-    base_url = "http://localhost:9002/";
+  assert = require('assert'),
+  demo1 = require("../server.js"),
+  base_url = "http://localhost:9002/";
 
-describe("DEMO_2 Server", function() {
-  describe("GET /", function() {
-    it("returns status code 200", function(done) {
-      request.get(base_url, function(error, response, body) {
+describe("DEMO_2 Server", function () {
+  describe("GET /", function () {
+    it("returns status code 200", function (done) {
+      request.get(base_url, function (error, response, body) {
         assert.equal(200, response.statusCode);
         done();
       });
     });
 
-    it("returns DEMO_2", function(done) {
-      request.get(base_url, function(error, response, body) {
-        assert.equal("DEMO_2", body);
+    it("returns DEMO_2", function (done) {
+      request.get(base_url, function (error, response, body) {
+        assert.equal("DEMO_2 on branch master", body);
         demo1.closeServer();
         done();
       });
