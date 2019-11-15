03d3a32ff91e44d61d8f5aee08fd3a3c4a6b774c
Lukasz Juraszek <juraszeklukasz@gmail.com>
2019-11-15 11:36:31 -0800

    test

--- packages/demo2/server.js
+++ packages/demo2/server.js
@@ -3,14 +3,14 @@ var app = express();
 var exports = module.exports = {};
 const PORT = 9002;
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.send('DEMO_2 on branch silly');
 });
 
-var server = app.listen(PORT, function(){
-  console.log(`Magic is happening on port ${PORT}`);
+var server = app.listen(PORT, function () {
+  console.log(` Magic is happening on port ${PORT}`);
 });
 
-exports.closeServer = function(){
+exports.closeServer = function () {
   server.close();
 };
